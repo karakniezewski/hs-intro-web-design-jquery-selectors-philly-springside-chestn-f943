0643e066058fbe80d46f87cbfaba44e34e8bcc26
karakniezewski <kkniezewski@sch.org>
2016-03-14 14:37:46 +0000

    nitrous-backup at Mon Mar 14 14:37:46 UTC 2016

--- js/selectors.js
+++ js/selectors.js
@@ -6,19 +6,26 @@ $(document).ready(function(){
   $('.special').addClass('pinky');
 
   // 1.) Select all <h1> and apply the class of pinky using addClass method.
-  
+  $('.Header 1').addClass('pinky');
+  $('.Header 2').addClass('pinky');
+  $('.Header 3').addClass('pinky');
+  $('.Header 4').addClass('pinky');
+  $('.Header 5').addClass('pinky');
+  $('.Header 6').addClass('pinky');
 
   // 2.) Select all <li> inside of <ol> only and apply the class of pinky using addClass method.
-  
+  $('.Crayon').addClass('pinky');
+  $('.Pencil').addClass('pinky');
+  $('.Stapler').addClass('pinky');
 
   // 3.) Select the <a> that is inside of the element that has both an id of box4 and a class of box5
-  
+  $('.box5').addClass('pinky')
 
   // 4.) Select the image of the cat that has the alt text equal to "cat sleeping"
-  
+
 
   // 5.) select only checked checkbox inputs and wrap them with a span that has the class of pinky using the wrap method.
-  
+
 
 
   // HINT: look at the documentation for:
